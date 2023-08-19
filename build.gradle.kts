b66b5f3ad104ad54ebf03abbec6b9a813c055725
jacky <jacky8399@protonmail.com>
2023-08-20 00:57:40 +0800

    use Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.jacky8399"
+version = "1.13.2"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "spigotmc-repo"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven("https://maven.enginehub.org/repo/")
+    mavenLocal()
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-chat:1.16-R0.4")
+    compileOnly("org.jetbrains:annotations:23.0.0")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.6")
+    implementation("org.bstats:bstats-bukkit:2.2.1")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    withType<ShadowJar> {
+        relocate("org.bstats", "com.jacky8399.portablebeacons.bstats")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
